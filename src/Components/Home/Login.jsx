e5d3e1d109cd6d8e56ac9218f0060e6fa1c67152
Niteesh <niteesh.21bce9461@vitapstudent.ac.in>
2023-11-04 15:34:21 +0530

    signup commit

--- src/Components/Home/Login.jsx
+++ src/Components/Home/Login.jsx
@@ -1,13 +1,15 @@
 /* eslint-disable react/prop-types */
 import { useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import Axios from "axios";
 
-
-const Login = ({ onClose,type, isPopupVisible  }) => {
+const Login = ({ onClose, type, isPopupVisible }) => {
   const [isLogin, setIsLogin] = useState(true);
+  const navigate = useNavigate();
   const [username, setUsername] = useState("");
   const [password, setPassword] = useState("");
   const [email, setEmail] = useState("");
-  const [mobile, setMobile] = useState("");
+  const [userDetails, setUserDetails] = useState([]);
 
   const handleToggleForm = () => {
     setIsLogin(!isLogin);
@@ -15,11 +17,41 @@ const Login = ({ onClose,type, isPopupVisible  }) => {
 
   const handleSubmit = (e) => {
     e.preventDefault();
+    Axios.get("http://localhost:4000/credentialRoute")
+      .then((res) => {
+        if (res.status === 200) setUserDetails(() => res.data);
+        else Promise.reject();
+      })
+      .catch((err) => alert(err));
+    const loggedInDetails = userDetails.map((userData) => {
+      userData?.email === email ? userData : null;
+    });
     if (isLogin) {
       // Perform login logic here
+      loggedInDetails === null ? alert("Please register before login") : navigate("/user");
     } else {
       // Perform registration logic here
       // You can access the values of `username`, `password`, `email`, and `mobile` for registration
+      const userData = {
+        userName: username,
+        password: password,
+        email: email,
+        type: type,
+      };
+      setUsername("");
+      setPassword("");
+      setEmail("");
+      setIsLogin(true);
+      loggedInDetails===null ?
+      Axios.post(
+        "http://localhost:4000/credentialRoute/create-credential",
+        userData
+      )
+        .then((res) => {
+          if (res.status === 200) alert("Registered successfully");
+          else Promise.reject();
+        })
+        .catch((err) => alert(err)) : alert("user already exists");
     }
   };
 
@@ -31,18 +63,27 @@ const Login = ({ onClose,type, isPopupVisible  }) => {
     >
       <div className="bg-white p-4 rounded-lg w-1/3">
         <h2 className="text-2xl font-bold mb-4 text-center">
-          {isLogin ? type+" Login" : type+ " Register"}
+          {isLogin ? type + " Login" : type + " Register"}
         </h2>
         <form onSubmit={handleSubmit}>
           <div className="mb-4">
-            <label className="block text-gray-700 text-sm font-semibold">
-              Username
-            </label>
+            {isLogin ? (
+              <label className="block text-gray-700 text-sm font-semibold">
+                Email
+              </label>
+            ) : (
+              <label className="block text-gray-700 text-sm font-semibold">
+                Username
+              </label>
+            )}
             <input
               type="text"
               className="w-full p-2 border rounded-lg"
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
+              value={isLogin ? email : username}
+              onChange={(e) =>
+                isLogin ? setEmail(e.target.value) : setUsername(e.target.value)
+              }
+              required
             />
           </div>
           <div className="mb-4">
@@ -54,6 +95,7 @@ const Login = ({ onClose,type, isPopupVisible  }) => {
               className="w-full p-2 border rounded-lg"
               value={password}
               onChange={(e) => setPassword(e.target.value)}
+              required
             />
           </div>
           {isLogin ? null : (
@@ -67,17 +109,7 @@ const Login = ({ onClose,type, isPopupVisible  }) => {
                   className="w-full p-2 border rounded-lg"
                   value={email}
                   onChange={(e) => setEmail(e.target.value)}
-                />
-              </div>
-              <div className="mb-4">
-                <label className="block text-gray-700 text-sm font-semibold">
-                  Mobile
-                </label>
-                <input
-                  type="text"
-                  className="w-full p-2 border rounded-lg"
-                  value={mobile}
-                  onChange={(e) => setMobile(e.target.value)}
+                  required
                 />
               </div>
             </>
